7d85c50c616510b99fe97c183e5aca2df4b4544b
Emanuel Ruiz <ruizemanuelok@gmail.com>
2023-05-27 17:17:28 -0300

    iniciar repo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Titulo from './components/Titulo'
+
+function App() {
+
+  return (
+    <>
+<Titulo tituloNuevo='My Friend'/>
+    </>
+  )
+  }
+export default App
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Titulo = (props) => {
+    return (
+
+        <>
+            <h1>Hello {props.tituloNuevo}</h1>
+        </>
+    );
+};
+
+export default Titulo;
